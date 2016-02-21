5a7aa60b3a675aa54ce8bc1ebe37464095cb58f0
AkonDer <vjacheslav.leb@gmail.com>
2016-02-21 16:03:47 +0500

    Реализованна инициализация переменных

--- ReCoord/RCoord.cs
+++ ReCoord/RCoord.cs
@@ -7,5 +7,24 @@ namespace ReCoord
 {
     public class RCoord
     {
+        int Xtr; // Координата Х траектории
+        int Xop; // Координата Х огневой позиции
+        int Yop; // Координата Y огневой позиции
+        int Xst; // Координата Х станции РЛС
+        int Yst; // Координата Y станции РЛС
+
+        public RCoord(  int xtr, // Координата Х траектории
+                        int xop, // Координата Х огневой позиции
+                        int yop, // Координата Y огневой позиции
+                        int xst, // Координата Х станции РЛС
+                        int yst) // Координата Y станции 
+        {
+            Xtr = xtr;
+            Xop = xop;
+            Yop = yop; // инициализация переменных
+            Xst = xst;
+            Yst = yst;
+        }
+
     }
 }
